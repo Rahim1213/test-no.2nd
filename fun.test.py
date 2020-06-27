29795e3a1ff3e89df3aa2f13e6ca9a4e4673e254
arrahim <arrahim89999@gmail.com>
2020-06-27 08:34:20 +0600

    creating for test

---
+++ fun.test.py
@@ -0,0 +1,9 @@
+name = input("Enter your name please\t")
+
+print("Ki miya khai dai kaj nai \U0001F60C\n ")
+bari = input("toh bari koi tmr!!!!\t")
+print("ohh tmr bari tailey " + bari + "\n""toh aikaney ki kam... \U0001F602\n")
+age = input("toh age koto akn !\t")
+print("hmm..onk age hoicey \n toh biyey sadi korey felo\n")
+kro = input ("toh ki kro akn  !!!\t\n")
+print("Acha  akn asi ami Allah Hafez")
---
+++ test 1.py
@@ -0,0 +1,5 @@
+print("\t hi")
+one =float(input("Enter you fist number\t"))
+two =float(input("Yup\n Now enter 2nd number\t"))
+total = one + two
+print("total is\t" + str(total) +"\n""total is printed by Floating number")
